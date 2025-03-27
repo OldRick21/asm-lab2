944ab81b10c2079b37e3276f0fca4047d6c58d06
siberian_rat <s1berian_rat@localhost>
2025-03-27 03:37:50 +0300

    God, how does this even work?

--- src.s
+++ src.s
@@ -1,19 +1,116 @@
 section .data
+    rows:
+        db 3
+    cols:
+        db 5
+    matrix:
+        dw 12, -5, 7, 3, 0
+        dw 4, 9, -2, 6, 1
+        dw -1, 8, 15, 2, -3
 section .text
 global _start
 _start:
-    jmp sort_matrix
+    mov r8, -1; костыль
 
-    mov eax, 60
-	mov	edi, 0
-	syscall
+rowloop:
+    add r8b, 1
+    cmp r8b, byte [rows]
+    jae _end ;jae >=
+    mov r9b, 0
+    jmp colloop_odd
 
-sort_matrix:
+
+colloop_odd:
+    mov r14, 1 ;флаг отвечающий за фазу чет\нечет
+    mov r12b, byte [cols] 
+    sub r12b, 1
+    cmp r9b, r12b
+    jae pre_colloop_even
+
+    movzx rax, r8b           ; Загружаем индекс строки в RAX
+    movsx rdi, byte [cols]     
+    imul rax, rdi
+    imul rax, 2               ; Умножаем на размер строки (10 байт)
+    movzx rbx, r9b           ; Загружаем индекс столбца в RBX
+    imul rbx, 2              ; Умножаем на размер элемента (2 байта)
+    mov si, word [matrix + rax + rbx]  ; Полный адрес
+
+;
+    movzx rax, r8b           
+    movsx rdi, byte [cols]     
+    imul rax, rdi
+    imul rax, 2             
+    movzx rbx, r9b          
+    imul rbx, 2              
+    mov cx, word [matrix + rax + rbx + 2] 
+
+    add r9b, 2 ;add r9b, 1
+
+    jmp cmp
+
+
+
+pre_colloop_even:
+    mov r9b, 1
+    jmp colloop_even
+
+
+colloop_even:
+    mov r14, 0 ;флаг отвечающий за фазу чет\нечет
+    mov r12b, byte [cols] 
+    sub r12b, 1
+    cmp r9b, r12b
+    jae rowloop
+
+    movzx rax, r8b           ; Загружаем индекс строки в RAX
+    movsx rdi, byte [cols]     
+    imul rax, rdi
+    imul rax, 2               ; Умножаем на размер строки (10 байт)
+    movzx rbx, r9b           ; Загружаем индекс столбца в RBX
+    imul rbx, 2              ; Умножаем на размер элемента (2 байта)
+    mov si, word [matrix + rax + rbx]  ; Полный адрес
+
+;
+    movzx rax, r8b      
+    movsx rdi, byte [cols]     
+    imul rax, rdi
+    imul rax, 2              
+    movzx rbx, r9b          
+    imul rbx, 2              
+    mov cx, word [matrix + rax + rbx + 2] 
+
+    add r9b, 2 ;add r9b, 1
+
+    jmp cmp
+
+cmp:
     %ifdef SORT_DESCENDING
-        jmp over_flow
+
+        cmp si, cx
+
     %else
-        jmp err
+
+        cmp cx, si 
+
     %endif
+
+    jg swap;первый больше 
+    jle not_swap;меньше или равен
+
+swap:
+    mov r9b, 0
+    mov word [matrix + rax + rbx + 2], si
+    mov word [matrix + rax + rbx], cx
+    jmp colloop_odd
+
+
+not_swap:
+    cmp r14, 1 ;флаг отвечающий за фазу чет\нечет
+    je colloop_odd
+    jmp colloop_even
+
+
+
 
 over_flow:
     mov	eax, 60
@@ -23,3 +120,7 @@ err:
     mov	eax, 60
     mov	edi, 2
     syscall
+_end:
+    mov eax, 60
+	mov	edi, 0
+	syscall
